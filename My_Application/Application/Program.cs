84cb3f5f6809f48e1bafc3c9913fb5c8e09ca2bb
Arvinmoj <arvinmojaverian@hotmail.com>
2022-07-30 11:29:36 +0430

    user logger class lib in program

--- My_Application/Application/Program.cs
+++ My_Application/Application/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Application
+{
+    public static class Program
+    {
+        static Program()
+        {
+        }
+
+        public static void Main()
+        {
+
+            Logger.Interfaces.ILogger 
+                logger = new Logger.Logger();
+
+            Logger.SomeClass someClass = new Logger.SomeClass(logger: logger);
+
+            string fullName = "Senior Martian";
+                someClass.DoSomething(fullName: fullName);
+            
+            Console.WriteLine("Press [Enter] To Exit ...");
+            Console.ReadLine();
+            
+        }
+    }
+}
